f0d1de0645a4f3bafc8cab9e5c869d9f833f33db
Sarawut Palawong <admin@257.com>
2025-09-02 03:54:57 +0000

    imprementcodeQL

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const add = (a, b) => {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new TypeError('Inputs must be numbers');
   }
-  const result= a+b;
-  return result;
+  //const result= a+b;
+  //return result;
+
+  //CodeQLCase
+  return eval('%{a} + %{b}');
 };
 export default add;
